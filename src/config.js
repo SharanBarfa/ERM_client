c4a7ddd2959e7604abf269742010737e14bfe57d
SharanBarfa <sharanbarfa2003@gmail.com>
2025-05-17 18:50:58 +0530

    sa

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_URL = 'https://erm-server.onrender.com';
+export const API_URL = 'https://erm-server.onrender.com/api';
  
 // Other configuration constants can be added here
 export const TOKEN_KEY = 'auth_token';
