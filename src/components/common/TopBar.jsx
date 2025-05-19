fef973002f4f3d8d111a1698bc58fda00b12d2c1
SharanBarfa <sharanbarfa2003@gmail.com>
2025-05-19 10:10:20 +0530

    topbar
    topbar  edit

--- src/components/common/TopBar.jsx
+++ src/components/common/TopBar.jsx
@@ -55,16 +55,17 @@ const TopBar = () => {
     ? [
         { name: 'Dashboard', path: '/admin/dashboard' },
         { name: 'Employees', path: '/admin/employees' },
-        { name: 'Teams', path: '/admin/teams' },
+        // { name: 'Teams', path: '/admin/teams' },
+      
         { name: 'Departments', path: '/admin/departments' },
-        { name: 'Project', path: '/admin/project' },
+        // { name: 'Project', path: '/admin/project' },
         { name: 'Attendance', path: '/admin/attendance' }
       ]
     : [
         { name: 'Dashboard', path: '/employee/dashboard' },
         { name: 'My Profile', path: '/employee/profile' },
-        { name: 'My Team', path: '/employee/team' },
-        { name: 'Projects', path: '/employee/projects' },
+        // { name: 'My Team', path: '/employee/team' },
+        // { name: 'Projects', path: '/employee/projects' },
         { name: 'Attendance', path: '/employee/attendance' }
       ];
 
